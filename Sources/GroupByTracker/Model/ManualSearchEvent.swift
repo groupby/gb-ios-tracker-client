eee3c89f67e1c4f3e780755edda35c1921aac61a
Mohammad Taif <MTaif@whitecapcanada.com>
2023-10-26 14:32:41 -0400

    test

--- Sources/GroupByTracker/Model/ManualSearchEvent.swift
+++ Sources/GroupByTracker/Model/ManualSearchEvent.swift
@@ -8,26 +8,7 @@ public class ManualSearchEvent: NSObject, NSCoding {
         self.googleAttributionToken = googleAttributionToken
         self.search = search
     }
-    
-    required init?(coder aDecoder: NSCoder) {
-        if let googleAttributionToken = aDecoder.decodeObject(forKey: "googleAttributionToken") as? String,
-           let search = aDecoder.decodeObject(forKey: "search") as? Search {
-            self.googleAttributionToken = googleAttributionToken
-            self.search = search
-        } else {
-            // Handle initialization failure
-            self.googleAttributionToken = ""
-            self.search = Search() // Assuming Search has a default initializer
-        }
-    }
-    
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(googleAttributionToken, forKey: "googleAttributionToken")
-        aCoder.encode(search, forKey: "search")
-    }
+
+
 }
 
-class Search: NSObject, NSCoding {
-    // Define the Search class if it's not already defined
-    // You need to implement NSCoding for Search as well
-}
